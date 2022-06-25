f97825cdea844adc51a23e38595dd27a4d8bc07a
hyeokjinJang <hjjang@dawin.xyz>
2022-06-25 23:01:22 +0900

    structure 세팅

---
+++ lib/src/config/themes/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        color: Colors.white,
+      ),
+      scaffoldBackgroundColor: Colors.white60,
+      primaryColor: Colors.black,
+    );
+  }
+}
